88f28db8375f41ae1f90bd1ff32220dff36e3006
Octaphous <pappkasse2121@gmail.com>
2022-01-29 16:18:31 +0100

    Fix pause menu esc problem, make menu black

--- Scenes/PauseMenu/PauseMenu.gd
+++ Scenes/PauseMenu/PauseMenu.gd
@@ -6,10 +6,9 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		if not get_tree().get_current_scene().get_name() == "Menu":
-		
+			
 			get_tree().paused = !get_tree().paused
 			visible = !visible;
-
 			$Center/Buttons/ContinueButton.grab_focus();
 
 func _on_ContinueButton_pressed():
