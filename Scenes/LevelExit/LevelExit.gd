7bbe7a0900d3315bbe25b2c1a105824540497897
Octaphous <pappkasse2121@gmail.com>
2022-01-29 13:44:07 +0100

    New Coffin Graffix

--- Scenes/LevelExit/LevelExit.gd
+++ Scenes/LevelExit/LevelExit.gd
@@ -2,8 +2,8 @@ extends Spatial
 
 func _OpenArea_entered(body):
 	$Animator.play("shake")
-	$Sprite.frame = 1
+	$SpriteCenter/Sprite.frame = 1
 
 func _OpenArea_exited(body):
 	$Animator.play("shake")
-	$Sprite.frame = 0
+	$SpriteCenter/Sprite.frame = 0
