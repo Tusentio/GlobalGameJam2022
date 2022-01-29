f11e264899d9f5b21653e1639b6c4f635b17210e
Keve1227 <killenlillen02@gmail.com>
2022-01-30 00:20:46 +0100

    Player dying

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,9 +1,14 @@
 extends KinematicBody
 
 
+signal dying
+signal dead
+
+
 var velocity = Vector3.ZERO
 var move_speed = 2
 var flipped = false
+var dead = false
 
 
 onready var animated_sprite: AnimatedSprite3D = $Pivot/Sprite
@@ -14,6 +19,8 @@ func _ready():
 
 
 func _physics_process(delta):
+	if dead: return
+	
 	# Input
 	velocity.x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
 	velocity.z = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
@@ -21,20 +28,42 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity.normalized() * move_speed, Vector3.UP)
 
 
-func _process(delta):
-	if velocity.x != 0:
+func _input(event):
+	if dead: return
+	
+	var x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
+	var z = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
+	
+	if x != 0:
 		animated_sprite.play("walk_sideways")
 		
-		if flipped and velocity.x > 0:
+		if flipped and x > 0:
 			flipped = false
 			$Animator.play("flip_right")
-		elif not flipped and velocity.x < 0:
+		elif not flipped and x < 0:
 			flipped = true
 			$Animator.play("flip_left")
-	elif velocity.z != 0:
-		if velocity.z < 0:
+	elif z != 0:
+		if z < 0:
 			animated_sprite.play("walk_yonder")
 		else:
 			animated_sprite.play("walk_hither")
 	else:
 		animated_sprite.play("idle")
+
+
+func kill(killer: Node = null):
+	if not dead:
+		dead = true
+		animated_sprite.play("ded")
+		emit_signal("dying", self, killer)
+
+
+func _on_Sprite_animation_finished():
+	if dead:
+		visible = false
+		emit_signal("dead", self)
+
+
+func _on_DeathrayListener_deathray_entered(deathray):
+	kill(deathray)
