ae31642bc2c1fc37da903fddaf0edf2158b895e4
Kevin Sundqvist Norlén <killenlillen02@gmail.com>
2022-01-29 20:48:45 +0100

    Better animation system

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,37 +1,44 @@
 extends KinematicBody
 
+
 var velocity = Vector3.ZERO
 var move_speed = 2
 var flipped = false
-var isRunning = false
+
+
+onready var animated_sprite: AnimatedSprite3D = $Pivot/Sprite
+
+
+func _ready():
+	animated_sprite.playing = true
+
 
 func _physics_process(delta):
 	# Input
 	velocity.x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
 	velocity.z = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
-
+	
 	velocity = move_and_slide(velocity.normalized() * move_speed, Vector3.UP)
 
-func _input(event):
-	var xfacing = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
-	var zfacing = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
-	var facing = xfacing - zfacing / 2
 
-	if facing != 0:
-		if not isRunning:
-			isRunning = true
-			$Pivot/Sprite.visible = false
-			$Pivot/Running.visible = true
-			$Pivot/Running.frame = 0
-			$Pivot/Running.playing = true
-		
-		if flipped and facing > 0:
+func _input(event):
+	var x_facing = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
+	var y_facing = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
+	
+	if x_facing != 0:
+		if flipped and x_facing > 0:
 			flipped = false
 			$Animator.play("flip_right")
-		elif not flipped and facing < 0:
+			animated_sprite.play("walk_right")
+		elif not flipped and x_facing < 0:
 			flipped = true
 			$Animator.play("flip_left")
-	elif isRunning:
-		isRunning = false
-		$Pivot/Sprite.visible = true
-		$Pivot/Running.visible = false
+			animated_sprite.play("walk_left")
+	elif y_facing != 0:
+		if y_facing < 0:
+			animated_sprite.play("walk_yonder")
+		else:
+			animated_sprite.play("walk_hither")
+	else:
+		animated_sprite.play("idle")
+		
