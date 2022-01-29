2864e1f99489462454ae8bf2af396b120b2423fb
Kevin Sundqvist Norlén <killenlillen02@gmail.com>
2022-01-29 10:30:09 +0100

    Turn on input event

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -6,15 +6,17 @@ var flipped = false;
 
 func _physics_process(delta):
 	# Input
-	velocity.x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left");	
+	velocity.x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left");
 	velocity.z = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up");
-	
-	# Animation
-	if velocity.x < 0 and not flipped:
-		flipped = true;
-		$Animator.play("flip_left");
-	elif velocity.x > 0 and flipped:
-		flipped = false;
-		$Animator.play("flip_right");
-	
+
 	velocity = move_and_slide(velocity.normalized() * move_speed, Vector3.UP);
+
+func _input(event):
+	var facing = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left");
+
+	if flipped and facing > 0:
+		flipped = false;
+		$Animator.play("flip_left");
+	elif not flipped and facing < 0:
+		flipped = true;
+		$Animator.play("flip_right");
