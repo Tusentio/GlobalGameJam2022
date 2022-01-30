d4b61dcdd82066dd070bd27be187766afc14c13f
Kevin Sundqvist Norlén <killenlillen02@gmail.com>
2022-01-30 13:15:02 +0100

    Make deathrays controllable by a switch

--- Scenes/Deathray/Deathray.gd
+++ Scenes/Deathray/Deathray.gd
@@ -1,10 +1,27 @@
 extends SpotLight
 
 
+export (NodePath) var switch: NodePath = ""
+export (bool) var on = true
+
+
 var listeners = []
+onready var _on = on
+
+
+func _ready():
+	if switch:
+		var switch_node = get_node(switch)
+		
+		if switch_node and switch_node.has_signal("switched"):
+			switch_node.connect("switched", self, "_on_Switch_switched")
 
 
 func _process(delta):
+	if on != _on:
+		turn_off() if on else turn_on()
+		_on = on
+	
 	var space_state = get_world().direct_space_state
 	
 	for listener in listeners:
@@ -25,6 +42,18 @@ func _process(delta):
 			listener.inside(self)
 
 
+func turn_off():
+	if on:
+		on = false
+		_on = false
+
+
+func turn_on():
+	if not on:
+		on = true
+		_on = true
+
+
 func _on_DeathArea_area_entered(area):
 	if area is DeathrayListener and not listeners.has(area):
 		listeners.append(area)
@@ -35,3 +64,7 @@ func _on_DeathArea_area_exited(area):
 		var listener = area as DeathrayListener
 		listener.outside(self)
 		listeners.erase(area)
+
+
+func _on_Switch_switched(state):
+	turn_on() if state else turn_off()
